3854cb2a1844479c47aab1a19ab459dd8e6d21bd
Shubham <shubham.rastogi@visionael.com>
2020-07-03 23:58:19 +0530

    Basic - Layout : Flexbox, positioning

--- App.js
+++ App.js
@@ -1,34 +1,65 @@
 import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Button,
-  Platform,
-  StatusBar,
-  View,
-} from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
   console.log("App Executed");
-  const { landscape } = useDeviceOrientation();
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        alignContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          width: 100,
+          height: 100,
+          //alignSelf: "flex-start",
+          //flex: 3,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: "absolute",
+          //flex: 2,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+          //alignSelf: "flex-end",
+          //flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+          //flex: 2,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          width: 100,
+          height: 100,
+          //flex: 2,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
