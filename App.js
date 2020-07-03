f9d4b7c0bf655c63ef2af1fb442021c0ab031167
Shubham <shubham.rastogi@visionael.com>
2020-07-04 00:58:40 +0530

    WelcomeScreen and ImageScreen

--- App.js
+++ App.js
@@ -1,65 +1,8 @@
 import React from "react";
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   console.log("App Executed");
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          //alignSelf: "flex-start",
-          //flex: 3,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-          //flex: 2,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          //alignSelf: "flex-end",
-          //flex: 1,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-          //flex: 2,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-          //flex: 2,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
