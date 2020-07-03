844a848281991eee69eaa4909e35806c6758f48f
Shubham <shubham.rastogi@visionael.com>
2020-07-03 20:31:14 +0530

    Basic - Styling based on Platform

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   console.log("App Executed");
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        color="black"
-        title="Click Me!!"
-        onPress={() => console.log("Button Clicked!!")}
-      />
       <Button title="Alert..." onPress={() => alert("Button Clicked!!")} />
-      <Button
-        title="Custom Alert"
-        onPress={() =>
-          Alert.alert("Title", "Message", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-      <Button
-        title="Prompt"
-        onPress={
-          () => Alert.prompt("Title", "Message", (text) => console.log(text)) //only works on ios
-        }
-      />
     </SafeAreaView>
   );
 }
@@ -34,7 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
