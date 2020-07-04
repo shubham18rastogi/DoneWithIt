fec2d41c668c56c4bf6af12a4ac499e9e9dc1521
Shubham <shubham.rastogi@visionael.com>
2020-07-04 23:15:26 +0530

    Icon support in ListItem

--- App.js
+++ App.js
@@ -8,8 +8,19 @@ import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   console.log("App Executed");
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <ListItem
+        title="Title"
+        subTitle="subTitle"
+        ImageComponent={<Icon name="email" />}
+      ></ListItem>
+    </Screen>
+  );
 }
