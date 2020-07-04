6c07e0aadd03e27a3c0d24ff42dfe6a441e132f9
Shubham <shubham.rastogi@visionael.com>
2020-07-04 11:51:38 +0530

    Custom Text Tag

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+
+import AppText from "./app/components/AppText";
 
 export default function App() {
   console.log("App Executed");
-  return <WelcomeScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>I Love React Native</AppText>
+    </View>
+  );
 }
