b8c29556ce7e99adddc37cf1d0eb45ddf706cbb1
Shubham <shubham.rastogi@visionael.com>
2020-07-04 12:05:08 +0530

    Platform Specific Code

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { StyleSheet, Text, Platform } from "react-native";
+
 function AppText({ children }) {
   return <Text style={styles.text}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "ios" ? "Avenir" : "Roboto",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
+    color: "tomato",
   },
 });
 
