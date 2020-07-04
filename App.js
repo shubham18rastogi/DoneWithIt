a918c03b3d12d70c5fff4129d48bacf7dcf9c446
Shubham <shubham.rastogi@visionael.com>
2020-07-04 12:29:27 +0530

    Custom Button Tag

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import { View } from "react-native";
 
 import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   console.log("App Executed");
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>I Love React Native</AppText>
+      <AppButton title="Login" onPress={() => console.log("Clicked")} />
     </View>
   );
 }
---
+++ app/components/AppButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import colors from "../config/colors";
+
+function AppButton({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+  },
+});
+
+export default AppButton;
