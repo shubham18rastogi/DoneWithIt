3f4796305f927e8ff2eba04c34e3141e88408115
Shubham <shubham.rastogi@visionael.com>
2020-07-05 23:03:12 +0530

    Custom ErrorMessage component

---
+++ app/components/ErrorMessage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppText from "./AppText";
+import { StyleSheet } from "react-native";
+
+function ErrorMessage({ error }) {
+  return error ? <AppText style={styles.error}>{error}</AppText> : null;
+}
+
+const styles = StyleSheet.create({
+  error: {
+    color: "red",
+  },
+});
+
+export default ErrorMessage;
