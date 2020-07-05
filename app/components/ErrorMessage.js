794c497609cc99662540976ceea59c851ff91542
Shubham <shubham.rastogi@visionael.com>
2020-07-05 23:16:18 +0530

    Touched state for input fields

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -2,8 +2,10 @@ import React from "react";
 import AppText from "./AppText";
 import { StyleSheet } from "react-native";
 
-function ErrorMessage({ error }) {
-  return error ? <AppText style={styles.error}>{error}</AppText> : null;
+function ErrorMessage({ error, visible }) {
+  return !error || !visible ? null : (
+    <AppText style={styles.error}>{error}</AppText>
+  );
 }
 
 const styles = StyleSheet.create({
