e4142ab7fd81551f47616f1cc0a4e4a39eae9fb0
Shubham <shubham.rastogi@visionael.com>
2020-07-09 00:01:49 +0530

    Read image from device and render it

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 
 import LoginScreen from "./app/screens/LoginScreen";
+import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState("");
   const requestPermission = async () => {
     const result = await ImagePicker.requestCameraPermissionsAsync();
     if (!result.granted)
@@ -14,5 +17,22 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <LoginScreen />;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image
+        source={{ uri: imageUri }}
+        style={{ margin: 10, width: 400, height: 400 }}
+      />
+    </Screen>
+  );
 }
