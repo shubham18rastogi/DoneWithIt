c5911da904c3b7e659a008c7c662cfee6e6697fe
Shubham <shubham.rastogi@visionael.com>
2020-07-08 23:35:47 +0530

    Requesting Camera permission using ImagePicker

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestCameraPermissionsAsync();
+    if (!result.granted)
+      alert("You need to enable permission to access the Library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return <LoginScreen />;
 }
