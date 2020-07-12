a53435fb8604238be3cd8c843c93b543a36272be
Shubham <shubham.rastogi@visionael.com>
2020-07-12 19:00:37 +0530

    ImageInput component

--- App.js
+++ App.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from "react";
-import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
 
-import LoginScreen from "./app/screens/LoginScreen";
 import Screen from "./app/components/Screen";
-import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState("");
-  const requestPermission = async () => {
-    const result = await ImagePicker.requestCameraPermissionsAsync();
-    if (!result.granted)
-      alert("You need to enable permission to access the Library");
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error reading an image", error);
-    }
-  };
+  const [imageUri, setImageUri] = useState();
 
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image
-        source={{ uri: imageUri }}
-        style={{ margin: 10, width: 400, height: 400 }}
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
       />
     </Screen>
   );
