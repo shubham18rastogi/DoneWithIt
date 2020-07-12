88d5e48e906764e8be9d6e55d10199c89f26d862
Shubham <shubham.rastogi@visionael.com>
2020-07-12 22:19:20 +0530

    Basic implementation of stack navigator

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Text, Button } from "react-native";
+import Screen from "./app/components/Screen";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="View Details"
+      onPress={() => navigation.navigate("TweetDetail")}
+    />
+  </Screen>
+);
+
+const TweetDetail = () => (
+  <Screen>
+    <Text>Tweet Detail</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetail" component={TweetDetail} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
