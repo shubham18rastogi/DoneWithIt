407313eb650c76ad92f7e27a7be4f491eb248e96
Shubham <shubham.rastogi@visionael.com>
2020-07-14 16:59:04 +0530

    Tracking upload progress

---
+++ app/screens/UploadScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View, Modal } from "react-native";
+import * as Progress from "react-native-progress";
+import AppText from "../components/Text";
+import colors from "../config/colors";
+
+function UploadScreen({ progress = 0, visible = false }) {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        <Progress.Bar color={colors.primary} progress={progress} />
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+
+export default UploadScreen;
