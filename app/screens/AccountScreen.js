62ea3e0b409fb006fafa5e89ce42743cd97efa69
Shubham <shubham.rastogi@visionael.com>
2020-07-15 00:52:13 +0530

    Populating logged in user info on accounts screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
 import { ListItem, ListItemSeparator } from "../components/lists";
 import colors from "../config/colors";
 import Icon from "../components/Icon";
 import Screen from "../components/Screen";
+import AuthContext from "./../auth/context";
 
 const menuItems = [
   {
@@ -25,12 +26,14 @@ const menuItems = [
 ];
 
 function AccountScreen({ navigation }) {
+  const { user } = useContext(AuthContext);
+
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
         <ListItem
-          title="Shubham Rastogi"
-          subTitle="shubham18rastogi@gmail.com"
+          title={user.name}
+          subTitle={user.email}
           image={require("../assets/shubham.jpg")}
         />
       </View>
