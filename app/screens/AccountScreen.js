4b43ff460ff17dbad34055c56556eb5918f0523f
Shubham <shubham.rastogi@visionael.com>
2020-07-15 10:26:03 +0530

    Persisting the login state

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -6,6 +6,7 @@ import colors from "../config/colors";
 import Icon from "../components/Icon";
 import Screen from "../components/Screen";
 import AuthContext from "./../auth/context";
+import authStorage from "../auth/storage";
 
 const menuItems = [
   {
@@ -27,6 +28,11 @@ const menuItems = [
 
 function AccountScreen({ navigation }) {
   const { user, setUser } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    setUser(null);
+    authStorage.removeToken();
+  };
 
   return (
     <Screen style={styles.screen}>
@@ -59,7 +65,7 @@ function AccountScreen({ navigation }) {
       <ListItem
         title="Log Out"
         IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
-        onPress={() => setUser(null)}
+        onPress={handleLogOut}
       />
     </Screen>
   );
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -12,6 +12,7 @@ import {
   SubmitButton,
   ErrorMessage,
 } from "../components/forms";
+import authStorage from "../auth/storage";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -28,6 +29,7 @@ function LoginScreen(props) {
     if (!result.ok) return setLoginFailed(true);
     const user = jwtDecode(result.data);
     authContext.setUser(user);
+    authStorage.storeToken(result.data);
   };
 
   return (
