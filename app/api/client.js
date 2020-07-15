ca00195230b2b97f7bf40477173d8a897ec3b14f
Shubham <shubham.rastogi@visionael.com>
2020-07-16 00:22:06 +0530

    Configurations for final build

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,10 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
 import authStorage from "../auth/storage";
+import settings from "../config/settings";
 
 const apiClient = create({
-  baseURL: "http://192.168.0.109:9000/api",
+  baseURL: settings.apiUrl,
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
@@ -24,4 +25,5 @@ apiClient.get = async (url, params, axiosConfig) => {
   const data = await cache.get(url);
   return data ? { ok: true, data } : response;
 };
+
 export default apiClient;
